fb15e08714baa03339f04785733e960a57cb5e68
irungu-ian <ian.irungu716@strathmore.edu>
2019-03-19 16:20:32 +0300

    Changed 3rd image in index page slider

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -129,52 +129,6 @@
                 <!-- Hotel Booking tab -->
                 <!-- Contact us details will be placed here rather than a form as detailed in the TODO.TODO -->
                 <div id="hotel" class="tab-pane fade">
-                    <form method="post" class="joc-form" style="display:none">
-                        <div class="row">
-                            <div class="col-md-2">
-                                <div class="booknow">
-                                    <h2>Book Now</h2>
-                                    <span>Best Price Online</span>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="date">Check-in:</label>
-                                    <div class="form-field">
-                                        <i class="icon icon-calendar2"></i>
-                                        <input type="text" id="date" class="form-control date" placeholder="Check-in date">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="date">Check-out:</label>
-                                    <div class="form-field">
-                                        <i class="icon icon-calendar2"></i>
-                                        <input type="text" id="date" class="form-control date" placeholder="Check-out date">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <div class="form-group">
-                                    <label for="guests">Guest</label>
-                                    <div class="form-field">
-                                        <i class="icon icon-arrow-down3"></i>
-                                        <select name="people" id="people" class="form-control">
-                                            <option value="#">1</option>
-                                            <option value="#">2</option>
-                                            <option value="#">3</option>
-                                            <option value="#">4</option>
-                                            <option value="#">5+</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <input type="submit" name="submit" id="submit" value="Find Hotel" class="btn btn-primary btn-block">
-                            </div>
-                        </div>
-                    </form>
                     <div class="row">
                         <form method="post" class="joc-form">
                             <div class="col-md-10 col-md-offset-1 animate-box" style="color:white">
@@ -185,7 +139,7 @@
                                             55016-00200 Nairobi, Kenya</p>
                                     </div>
                                     <div class="col-md-3">
-                                        <p><i class="icon-phone2" style="padding-right: 0.4em;"></i>(+254) 786 336 633</p>
+                                        <p><i class="icon-phone2" style="padding-right: 0.4em;"></i><a href="tel:+254786336633"  style="color:#E4A300">(+254) 786 336 633</a></p>
                                     </div>
                                     <div class="col-md-3">
                                         <p><i class="icon-mail5" style="padding-right: 0.4em;"></i><a href="mailto:info@ziaratravels.co.ke"
